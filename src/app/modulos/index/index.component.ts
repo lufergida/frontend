ec13470e377213da39a98e5cf25d11feb23699e1
Juan Jose Restrepo <juanjose.restrepo@vivaair.com>
2022-12-16 10:53:29 -0500

    API example

--- src/app/modulos/index/index.component.ts
+++ src/app/modulos/index/index.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-index',
   templateUrl: './index.component.html',
   styleUrls: ['./index.component.scss']
 })
-export class IndexComponent {
 
+
+export class IndexComponent implements OnInit {
+  data = [];
+
+  constructor(
+    private http : HttpClient
+  ){ } 
+
+  ngOnInit(): void {
+    this.getUsers();
+  }
+
+  public getUsers() {
+    this.http.get<any>('https://reqres.in/api/users?page=1').subscribe(
+      response => {
+        console.log(response)
+        this.data= response;
+          }
+      )
+  }
 }
